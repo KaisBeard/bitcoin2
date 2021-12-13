8b920c0eabc717a965ef74373e40e66d40ce5d4c
555ekstase <martinbibow@gmx.de>
2021-12-13 15:13:35 +0100

    added something

--- src/App.js
+++ src/App.js
@@ -2,87 +2,80 @@ import './App.css';
 import React from "react";
 import {useState, useEffect} from "react";
 import axios from "axios";
-<<<<<<< HEAD
 import Coin from "./Coin";
-=======
-import Coin from "./Coin"
->>>>>>> 0d289cb8d5207893b2df4316395493d4aa30d2d0
 
 function App() {
   const [coins, setCoins] = useState ([])
   const [search, setSearch]= useState("")
 
-useEffect(() => {
-  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=EUR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-  .then(res =>
-    {setCoins(res.data);
-  }).catch(error => console.log(error))
-}, []);
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>
+      {setCoins(res.data);
+    }).catch(error => console.log(error))
+  }, []);
+
+  console.log(coins);
 
   const handleChange = e => {
-
     setSearch(e.target.value)
   }
 
-const filteredCoins = coins.filter(coin =>
-  coin.name.toLowerCase().includes(search.toLowerCase())
-  )
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
 
   return (
-    <div className="App">
-      <div className="coin-search">
-      <h1 className="coin-text">Kryptowährung suchen</h1>
-      <form>
-        <input type="text" placeholder="Suche Kryptowährung" className='coin-input'/>
-      </form>
-    </div>
+    <div>
+      <div className="App">
+        <div className="coin-search">
+          <h1 className="coin-text">Kryptowährung suchen</h1>
+          <form>
+            <input type="text" placeholder="Suche Kryptowährung" className='coin-input' onChange = {handleChange}  />
+          </form>
+        </div>
+      </div>     
     
 
-
-
-
-
-
-
-
-
-    
-<div className="App">
-      <div className="coin-search">
-      <h1 className="coin-text">Kryptowährung suchen</h1>
-      <form>
-        <input type="text" placeholder="Suche Kryptowährung" className='coin-input' onChange = {handleChange}  />
-      </form>
-    </div>
-
-    {filteredCoins}.map(coin => {
-      return (
-        <Coin 
-        key={coin.id} 
-        name={coin.name} 
-        image={coin.image}
-        symbol={coin.symbol}
-        volume={coin.market_cap}
-        price={coin.current_price}
-        priceChange={coin.price_change_percentage_24h}
-        marketcap={coin.total_volume}
-        />
-      );
-    })
+      {filteredCoins.map(coin => {
+        return (
+          <Coin 
+              key={coin.id} 
+              name={coin.name} 
+              image={coin.image}
+              symbol={coin.symbol}
+              volume={coin.market_cap}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              marketcap={coin.total_volume}
+              />
+          );
+      })}
     </div>
   );
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export default App;
+
+
+/*
+
+ {filteredCoins.map(coin => {
+        return (
+          <div>hallo</div>
+      
+
+<Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          marketcap={coin.total_volume}
+          />
+  );
+      })}
+
+*/
--- src/Coin.js
+++ src/Coin.js
@@ -1,45 +1,39 @@
-
-
-const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap}) => {
-
-
+import React from 'react';
+import "./Coin.css";
 
 
 
 
-
-
-
-
-<p className='coin-volume'>${volume.toLocaleString()}</p>
-{priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
-          )}
-         <p className='coin-marketcap'>
-            Mkt Cap: ${marketcap.toLocaleString()}
-          </p>
-import React from 'react'
-
-export default function Coin({name, image, symbol,price, volume}) {
-    return (
-        <div className="coin-container">
-            <div className="coin-row">
-<div className="coin">
-    <img sc={image} alt="crypto" />
-    <h1>{name}</h1>
-    <p className= "coin-symbol" > {symbol} </p>
-
-</div>
-
-<div className="coin-data">
-    <p className="coin-price">$ {price}  </p>
-    <p className="coin-volume">$ {volume.toLocaleString()}  </p>
-
+export default function Coin({name, image, symbol, price, priceChange, marketcap, volume}) {
     
-     </div>
+    //const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap}) => {
+
+    return (
+        <div>
+            <div className='coin-volume'>${volume.toLocaleString()}</div>
+            {priceChange < 0 ? (
+                        <div className='coin-percent red'>{priceChange.toFixed(2)}%</div>
+                    ) : (
+                        <div className='coin-percent green'>{priceChange.toFixed(2)}%</div>
+                    )}
+                    <div className='coin-marketcap'> Mkt Cap: ${marketcap.toLocaleString()}</div>
+
+
+
+            <div className="coin-container">
+                <div className="coin-row">
+                    <div className="coin">
+                        <img sc={image} alt="crypto" />
+                        <h1>{name}</h1>
+                        <p className= "coin-symbol" > {symbol} </p>
+                    </div>
+
+                    <div className="coin-data">
+                        <div className="coin-price">$ {price}  </div>
+                        <div className="coin-volume">$ {volume.toLocaleString()}  </div>        
+                    </div>
+                </div>
             </div>
-        </div>
+        </div>            
     )
 }
