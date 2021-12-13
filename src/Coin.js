1bbf44045a53d52545bf3567830a976623695752
555ekstase <martinbibow@gmx.de>
2021-12-13 16:17:13 +0100

    v2

--- src/Coin.js
+++ src/Coin.js
@@ -4,36 +4,24 @@ import "./Coin.css";
 
 
 
-export default function Coin({name, image, symbol, price, priceChange, marketcap, volume}) {
+export default function Coin({name, symbol, price, priceChange, marketcap, volume}) {
     
     //const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap}) => {
 
     return (
         <div>
-            <div className='coin-volume'>${volume.toLocaleString()}</div>
+            <h2>{name}</h2>
+        <div className="gridParent">
+            <div className='coin-volume'> Marktvolumen: EUR <div> {volume.toLocaleString()}</div></div>
             {priceChange < 0 ? (
-                        <div className='coin-percent red'>{priceChange.toFixed(2)}%</div>
+                        <div className='coin-percent'> Entwicklung 24h: <div className='coin-percent red'>{priceChange.toFixed(2)}%</div></div>
                     ) : (
-                        <div className='coin-percent green'>{priceChange.toFixed(2)}%</div>
+                        <div className='coin-percent'> Entwicklung 24h:<div className='coin-percent green'>{priceChange.toFixed(2)}%</div></div>
                     )}
-                    <div className='coin-marketcap'> Mkt Cap: ${marketcap.toLocaleString()}</div>
-
-
-
-            <div className="coin-container">
-                <div className="coin-row">
-                    <div className="coin">
-                        <img sc={image} alt="crypto" />
-                        <h1>{name}</h1>
-                        <p className= "coin-symbol" > {symbol} </p>
-                    </div>
-
-                    <div className="coin-data">
-                        <div className="coin-price">$ {price}  </div>
-                        <div className="coin-volume">$ {volume.toLocaleString()}  </div>        
-                    </div>
-                </div>
-            </div>
+                    <div className='coin-marketcap'>    Mkt Cap: EUR    <div>{marketcap.toLocaleString()}</div></div>           
+            <div className="coin-price">Aktueller Kurs: EUR <div>{price}  </div></div>
+            {/* <div className="coin-volume"> EUR {volume.toLocaleString()}  </div>         */}
+        </div>
         </div>            
     )
 }
